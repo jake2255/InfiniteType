9ddd873ee5dec52b9e0605e96e98dd99bce54559
jake2255 <157094404+jake2255@users.noreply.github.com>
2026-07-19 11:46:27 -0500

    added some missing django pages

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Account(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    lifetime_words = models.PositiveIntegerField(default=0)
+    
+    def __str__(self):
+        return self.user.username
